d03536c19d45ceb487e50cee1422c298d2d0423b
Vhens <942428642@qq.com>
2017-04-05 17:28:31 +0800

    'echart'

--- vitae/js/vitae.js
+++ vitae/js/vitae.js
@@ -1,4 +1,20 @@
 (function(){
     var bg_h=$(window).height();
     $('.information,.about,.technology,.project,.contact,#myBg').css('height',bg_h);
+    $('#tagsList .item').hover(function(){
+        $(this).css('color','#'+numRadom());
+    },function(){
+         $(this).css('color','#fff');
+    })
+     // $('#tagsList .item').on('click',function(){
+     //    alert(1)
+     // })
+    //随机6位数
+    function numRadom(){
+        var num="";
+        for(var i=0;i<6;i++){
+            num+=Math.floor(Math.random()*10);
+        }
+        return num;
+    }
 })();
