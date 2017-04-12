17188faf4a8c8220fe7c0b7860b3f23762657386
Vhens <942428642@qq.com>
2017-04-12 14:22:07 +0800

    'aaa'

--- vitae/js/vitae.js
+++ vitae/js/vitae.js
@@ -17,6 +17,20 @@
         }
         return num;
     }
+//     setInterval(fn,1000)
+//     function fn(){
+//         var x=document.body.scrollTop;
+// alert(x)
+//     }
+window.onscroll=function(){
+    var x=document.body.scrollTop;
+
+    if(0<x<10){
+        // alert(x)
+
+        console.log(1)
+    }
+}
     //right圆点
     // $('#cricle li').on('click',function(){
     //     $(this).addClass('cur').siblings().removeClass('cur');
